57a9eb7e4232236f1ec386ecaaecfe141ce55a72
shiuantsai <a5204860@gmail.com>
2024-08-27 21:26:56 +0800

    Update CSA.v

--- 4_CSA/CSA.v
+++ 4_CSA/CSA.v
@@ -13,6 +13,7 @@ fulladder fa_inst11(x[1],y[1],z[1],s1[1],c1[1]);
 fulladder fa_inst12(x[2],y[2],z[2],s1[2],c1[2]);
 fulladder fa_inst13(x[3],y[3],z[3],s1[3],c1[3]); 
 
+        //c1需向左移一位，故c1[0]相當於跑去跟s1[1]相加
 fulladder fa_inst20(s1[1],c1[0],1'b0,s[1],c2[1]);
 fulladder fa_inst21(s1[2],c1[1],c2[1],s[2],c2[2]);
 fulladder fa_inst22(s1[3],c1[2],c2[2],s[3],c2[3]);
